ad45822bfd4b783bd587d36f9bcb7e7faa6c4c9c
Kellye Greene <kellye@gust.com>
2016-06-07 22:14:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,5 +4,22 @@ def nyc_pigeon_organizer(data)
   names = data[:color].values
   names.flatten!
   names.uniq!
-  binding.pry
+
+  pigeon_list = {}
+
+  names.map do |name|
+    pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+  end
+
+
+  data.each do |keys, values|
+    values.each do |property, pigeons|
+      pigeons.each do |pigeon|
+        if pigeon_list[pigeon][keys].include?(property) == false
+          pigeon_list[pigeon][keys] << property.to_s
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
